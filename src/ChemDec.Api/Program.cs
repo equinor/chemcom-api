c7a07e12d731cb7e1ba7928247b31c198b0d8404
Fredrik Wold <fredrikmikalwold@gmail.com>
2023-09-13 12:40:11 +0200

    fix(Program.cs): update localhost port from 5173 to 5174 in CORS origins to align with updated local development settings

--- src/ChemDec.Api/Program.cs
+++ src/ChemDec.Api/Program.cs
@@ -77,7 +77,7 @@ builder.Services.AddCors(options =>
         {
             "https://frontend-chemcom-dev.radix.equinor.com",
             "https://frontend-chemcom-prod.radix.equinor.com",
-            "http://localhost:5173",
+            "http://localhost:5174",
             "https://chemcom.equinor.com"
         };
         builder.WithOrigins(origins)
