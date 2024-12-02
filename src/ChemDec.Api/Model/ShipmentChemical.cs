b5f1bdf60a2fab26f42e2ba5229a37c6bd8a8172
Surya Prakash <supra@equinor.com>
2024-12-02 12:13:21 +0530

    fix: calulatedweight is always 0 (#191)
    removed fields from backend as they are not needed in payload (#189)

--- src/ChemDec.Api/Model/ShipmentChemical.cs
+++ src/ChemDec.Api/Model/ShipmentChemical.cs
@@ -12,20 +12,6 @@ namespace ChemDec.Api.Model
         public string MeasureUnit { get; set; }
         public double Amount { get; set; }
 
-        public double CalculatedWeightUnrinsed { get; set; } //in kg
-        public double CalculatedTocUnrinsed { get; set; } //in kg
-        public double CalculatedNitrogenUnrinsed { get; set; } //in kg
-        public double CalculatedBiocidesUnrinsed { get; set; } //in kg
-
-
-        public double CalculatedWeight { get; set; } //in kg
-        public double CalculatedToc { get; set; } //in kg
-        public double CalculatedNitrogen { get; set; } //in kg
-        public double CalculatedBiocides { get; set; } //in kg 
-
-        public DateTime Updated { get; set; }
-        public string UpdatedBy { get; set; }
-
     }
     public class ShipmentChemicalProfile : Profile
     {
