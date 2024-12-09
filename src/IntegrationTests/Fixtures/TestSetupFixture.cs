60ac15b81d693f3e151caf1ab006496b137efac9
Surya Prakash <SUPRA@equinor.com>
2024-12-09 17:55:31 +0530

    Resolved warnings

--- src/IntegrationTests/Fixtures/TestSetupFixture.cs
+++ src/IntegrationTests/Fixtures/TestSetupFixture.cs
@@ -153,7 +153,7 @@ public class TestSetupFixture : IAsyncLifetime, IDisposable
 
     public async Task InitializeAsync()
     {
-        await Task.Delay(1).ConfigureAwait(false);
+        await Task.CompletedTask;
     }
 
     public async Task DisposeAsync()
